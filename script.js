8a8f6765411c43b74406a7099d2f8b46f6a885c3
raghidelmiari <raghid1e1mairi@gmail.com>
2026-02-09 18:46:08 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,74 @@
+$(document).ready(function () {
+
+    const loveLanguages = [
+        "Mio mare ti amo", "I love you", "Je t’aime", "Te amo",
+        "Ich liebe dich", "Ti amo", "Eu te amo", "أحبك",
+        "Я тебя люблю", "Σ’ αγαπώ", "אני אוהב אותך",
+        "我爱你", "愛してる", "사랑해", "Seni seviyorum",
+        "Ik hou van jou", "Jag älskar dig", "Volim te",
+        "Te iubesc", "Kocham cię", "Saya cinta kamu",
+        "Mahal kita", "Nakupenda", "Mo nifẹ rẹ",
+        "Anh yêu em", "Is breá liom tú", "Mi amas vin",
+        "Habibi ana bahebak", "Ana behibek",
+        "Mio mare ti amo 💖"
+    ];
+// PASSWORD (change it here 💖)
+const SECRET_PASSWORD = "mio mare";
+
+$("#unlockBtn").click(function () {
+    const entered = $("#passwordInput").val();
+    if (entered === SECRET_PASSWORD) {
+        $("#lockScreen").fadeOut(800);
+        $("#mainContent").fadeIn(800);
+        document.getElementById("bgMusic").play();
+    } else {
+        $("#errorMsg").fadeIn().delay(1200).fadeOut();
+    }
+});
+    let index = 0;
+
+    setInterval(() => {
+        $("#loveText").fadeOut(400, function () {
+            $(this).text(loveLanguages[index]).fadeIn(400);
+        });
+        index = (index + 1) % loveLanguages.length;
+    }, 2000);
+
+    // Floating hearts
+    setInterval(() => {
+        const heart = $("<span>❤</span>");
+        heart.css({
+            left: Math.random() * 100 + "vw",
+            fontSize: Math.random() * 20 + 15 + "px",
+            animationDuration: Math.random() * 4 + 6 + "s"
+        });
+        $(".hearts").append(heart);
+        setTimeout(() => heart.remove(), 8000);
+    }, 300);
+
+    // Photo upload
+    $("#photoInput").on("change", function () {
+        const file = this.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = function (e) {
+                $("#photoPreview").attr("src", e.target.result).fadeIn(600);
+                $("#uploadText").hide();
+            };
+            reader.readAsDataURL(file);
+        }
+    });
+
+    // Music toggle
+    const music = document.getElementById("bgMusic");
+    $("#musicBtn").click(function () {
+        if (music.paused) {
+            music.play();
+            $(this).text("🔊 Music");
+        } else {
+            music.pause();
+            $(this).text("🔇 Music");
+        }
+    });
+
+});
